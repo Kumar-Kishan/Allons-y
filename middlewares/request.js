32d9d67886dc4e779b86dc0e1502ba1c646c952e
Kishan Kumar <kumar.kishan@outlook.com>
2018-12-25 23:16:14 +0545

    More flexible pagination

--- middlewares/request.js
+++ middlewares/request.js
@@ -1,8 +1,8 @@
 export default {
 	SetupPaging: (req,res,next) => {
-		req.body.perPage = req.body.perPage ? parseInt(req.body.perPage) : (parseInt(process.env.PER_PAGE) || 10)
+		req.body.count = req.body.count ? parseInt(req.body.count) : (parseInt(process.env.PER_PAGE) || 10)
 		req.body.pageNo = req.body.pageNo ? parseInt(req.body.pageNo) : 1
-		req.body.skip = req.body.skip ? parseInt(req.body.skip) : (req.body.pageNo  - 1) * req.body.perPage
+		req.body.skip = req.body.skip ? parseInt(req.body.skip) : (req.body.pageNo  - 1) * req.body.count
 		next()
 	},
 	ParamsToBody: (req,res,next) => {
